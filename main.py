dedb04c579fcd153064e48ffcfed2ba73cd3068d
Radoslaw Ptak <radek1026@interi.pl>
2025-10-22 20:48:22 +0200

    CR change in implement py-game-models

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ def main() -> None:
             guild_obj, _ = (
                 Guild.objects.get_or_create(
                     name=guild_info["name"],
-                    defaults={"description": guild_info.get("description", "")}))
+                    defaults={
+                        "description": guild_info.get("description", "")
+                    })
+            )
 
         player_obj, created = (
             Player.objects.get_or_create(
@@ -37,15 +40,37 @@ def main() -> None:
         )
 
         if not created:
-            if "email" in player and player["email"] is not None:
-                player_obj.email = player["email"]
-            if "bio" in player and player["bio"] is not None:
-                player_obj.bio = player["bio"]
-            if "race" in player:
+            email = player.get("email")
+            if email is not None:
+                player_obj.email = email
+            bio = player.get("bio")
+            if bio is not None:
+                player_obj.bio = bio
+            if player.get("race") is not None:
                 player_obj.race = race_obj
-            if "guild" in player:
+            if player.get("guild") is not None:
                 player_obj.guild = guild_obj
             player_obj.save()
+
+        skills = race_info.get("skills", [])
+
+        for skill in skills:
+            name = skill.get("name")
+            bonus = skill.get("bonus")
+            if not name or not bonus:
+                continue
+            skill_obj, created = Skill.objects.get_or_create(
+                name=name,
+                defaults={"bonus": bonus, "race": race_obj}
+            )
+
+            if not created:
+                changed = False
+                if skill_obj.bonus != bonus:
+                    skill_obj.bonus = bonus
+                    changed = True
+                if changed:
+                    skill_obj.save()
 
 
 if __name__ == "__main__":
